f5c43a483cfb509c455be7aa2d3de8f3322723aa
rumslaw <rumslaw@gmail.com>
2021-01-16 08:25:29 +0100

    Reformatting code

--- src/main/java/pl/blogservice/model/BlogPost.java
+++ src/main/java/pl/blogservice/model/BlogPost.java
@@ -9,12 +9,18 @@ import java.util.List;
 @Data
 public class BlogPost {
 
-    private final @NonNull long id;
-    private final @NonNull User author;
-    private final @NonNull String title;
-    private final @NonNull String content;
-    private final @NonNull Topic topic;
-    private final @NonNull LocalDateTime created;
+    private final @NonNull
+    long id;
+    private final @NonNull
+    User author;
+    private final @NonNull
+    String title;
+    private final @NonNull
+    String content;
+    private final @NonNull
+    Topic topic;
+    private final @NonNull
+    LocalDateTime created;
     private LocalDateTime modified;
     private List<Comment> comments;
 
